0fac0adb1940a478dc7213be5238d50653c8773c
Lívio Lopes <antonio.lvio760@gmail.com>
2023-07-04 18:12:14 -0300

    feat: create utils with status code and status messages

---
+++ backend/src/utils/statusUtils.js
@@ -0,0 +1,13 @@
+const statusCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+};
+
+const statusMessage = {
+  PRODUCT_NOT_FOUND: { message: 'Product not found' },
+};
+
+module.exports = {
+  statusCode,
+  statusMessage,
+};
