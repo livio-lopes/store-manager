c8d5d79dcb12844d967c74665b7dee6eac07e682
Lívio Lopes <antonio.lvio760@gmail.com>
2023-07-04 18:14:19 -0300

    feat: create controller getProductById

--- backend/src/controllers/products.controller.js
+++ backend/src/controllers/products.controller.js
@@ -1,12 +1,20 @@
 const productsServices = require('../services/products.service');
-
-const OK = 200;
+const { statusCode, statusMessage } = require('../utils/statusUtils');
 
 const getAllProducts = async (_req, res) => {
   const listProducts = await productsServices.getAllProducts();
-  return res.status(OK).json(listProducts);
+  return res.status(statusCode.OK).json(listProducts);
+};
+
+const getProductById = async (req, res) => {
+  const { id } = req.params;
+  const product = await productsServices.getProductById(id);
+  if (product) return res.status(statusCode.OK).json(product);
+  return res.status(statusCode.NOT_FOUND)
+  .json(statusMessage.PRODUCT_NOT_FOUND);
 };
 
 module.exports = {
   getAllProducts,
+  getProductById,
 };
