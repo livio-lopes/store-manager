20d847b267213919003b4236116183b984407bd0
Lívio Lopes <antonio.lvio760@gmail.com>
2023-07-04 16:44:59 -0300

    feat: create mocks with all products

---
+++ backend/tests/unit/mocks/productsMocks.js
@@ -0,0 +1,16 @@
+  const getAllProducts = [
+    {
+      id: 1,
+      name: 'Martelo de Thor',
+    },
+    {
+      id: 2,
+      name: 'Traje de encolhimento',
+    },
+    {
+      id: 3,
+      name: 'Escudo do CapitÃ£o AmÃ©rica',
+    },
+  ];
+
+module.exports = { getAllProducts };
