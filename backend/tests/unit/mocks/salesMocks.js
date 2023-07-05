bf1c1f018d8e075ea03b7b3402d5a2cd1ded43b2
Lívio Lopes <antonio.lvio760@gmail.com>
2023-07-05 15:50:26 -0300

    feat: create salesMocks

---
+++ backend/tests/unit/mocks/salesMocks.js
@@ -0,0 +1,14 @@
+const getAllSales = [
+    {
+      id: 1,
+      date: '2023-06-29T19:15:11.000Z',
+    },
+    {
+      id: 2,
+      date: '2023-06-29T19:15:11.000Z',
+    },
+  ];
+
+  module.exports = {
+    getAllSales,
+  };
