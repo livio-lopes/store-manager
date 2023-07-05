187723f51bdcd754e3c8e792e739055d19e5e835
Lívio Lopes <antonio.lvio760@gmail.com>
2023-07-04 23:31:22 -0300

    test: test getProductById on controller layer

--- backend/tests/unit/controllers/products.controller.test.js
+++ backend/tests/unit/controllers/products.controller.test.js
@@ -3,7 +3,7 @@ const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 const productsServices = require('../../../src/services/products.service');
 const productsController = require('../../../src/controllers/products.controller');
-const { getAllProducts } = require('../mocks/productsMocks');
+const productsMocks = require('../mocks/productsMocks');
 
 const { expect } = chai;
 
@@ -11,18 +11,28 @@ const OK = 200;
 
 chai.use(sinonChai);
 
-describe('Testa camada controller', function () {
-    it('Testa retorno da camada de controle', async function () {
+describe('Test Controller Layer', function () {
+    it('Test return from getAllProducts on controller layer', async function () {
       const req = {};
       const res = {
         status: sinon.stub().returnsThis(),
         json: sinon.stub(),
       };
-      sinon.stub(productsServices, 'getAllProducts').resolves(getAllProducts);
-     await productsController.getAllProducts(req, res);
+      sinon.stub(productsServices, 'getAllProducts').resolves(productsMocks.getAllProducts);
+      await productsController.getAllProducts(req, res);
       expect(res.status).to.have.been.calledWith(OK);
-    
-      expect(res.json).to.have.been.calledWith(getAllProducts);
+      expect(res.json).to.have.been.calledWith(productsMocks.getAllProducts);
+    });
+    it('Test return from getProductById with id = 3', async function () {
+      const req = { params: { id: '3' } };
+      const res = {
+        status: sinon.stub().returnsThis(),
+        json: sinon.stub(),
+      };
+      sinon.stub(productsServices, 'getProductById').resolves(productsMocks.getProductById3);
+      await productsController.getProductById(req, res);
+      expect(res.status).to.have.been.calledWith(OK);
+      expect(res.json).to.have.been.calledWith(productsMocks.getProductById3);
     });
     afterEach(function () {
       sinon.restore();
