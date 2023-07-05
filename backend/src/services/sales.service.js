1a62557bf28d90b6ec73d71d781d72fc23fb9eca
Lívio Lopes <antonio.lvio760@gmail.com>
2023-07-05 00:42:12 -0300

    feat: create getAllSales on sales.service

---
+++ backend/src/services/sales.service.js
@@ -0,0 +1,8 @@
+const salesModel = require('../models/sales.model');
+
+const getAllSales = async () => {
+    const sales = await salesModel.getAllSales();
+    return sales;
+  };
+
+module.exports = { getAllSales };
