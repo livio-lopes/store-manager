4172f22c0f8e4d9175ffb5ec18bb779b05a91a22
Lívio Lopes <antonio.lvio760@gmail.com>
2023-07-06 14:25:35 -0300

    fix: resolves bug of function from utils deleted

--- backend/src/models/products.model.js
+++ backend/src/models/products.model.js
@@ -1,16 +1,13 @@
 const connection = require('./connection');
-const storeManager = require('./utils/storeManagerTable');
-
-const PRODUCTS = 'products';
 
 const getAllProducts = async () => {
-  const [products] = await connection.execute(`SELECT * FROM ${storeManager(PRODUCTS)}`);
+  const [products] = await connection.execute('SELECT * FROM StoreManager.products');
   return products;
 };
 
 const getProductById = async (id) => {
   const [[product]] = await connection
-  .execute(`SELECT * FROM ${storeManager(PRODUCTS)} WHERE id =?`, [id]);
+  .execute('SELECT * FROM StoreManager.products WHERE id =?', [id]);
   return product;
 };
 
