ee80049b2eedf2f887c1759789b1e95de778d78a
Lívio Lopes <antonio.lvio760@gmail.com>
2023-07-06 10:43:37 -0300

    feat: add middleware express.json

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routers = require('./routes');
 
 const app = express();
-
+app.use(express.json());
 app.use(routers);
 
 // não remova esse endpoint, é para o avaliador funcionar
