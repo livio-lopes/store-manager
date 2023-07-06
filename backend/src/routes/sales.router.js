884bbf416d48131416e18e46527a6b5ef678d98c
Lívio Lopes <antonio.lvio760@gmail.com>
2023-07-06 14:26:24 -0300

    feat: create endpoint POST /sales

--- backend/src/routes/sales.router.js
+++ backend/src/routes/sales.router.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.get('/', salesController.getAllSales);
 router.get('/:id', salesController.getSalesById);
-router.post('/', salesController);
+router.post('/', salesController.setNewSales);
 
 module.exports = router;
