99a1560e97db45475fcbfa645d04acc68bab633e
Lívio Lopes <antonio.lvio760@gmail.com>
2023-07-06 14:29:18 -0300

    feat: create registerSales on service layer

--- backend/src/services/sales.service.js
+++ backend/src/services/sales.service.js
@@ -1,17 +1,27 @@
 const salesModel = require('../models/sales.model');
 
 const getAllSales = async () => {
-    const sales = await salesModel.getAllSales();
-    return sales;
+  const sales = await salesModel.getAllSales();
+  return sales;
 };
 
 const getSalesById = async (id) => {
-    const sale = await salesModel.getSalesById(id);
-    return sale.length === 0 ? undefined
-     : sale.map(({ date, productId, quantity }) => ({ date, productId, quantity }));
-  };
+  const sale = await salesModel.getSalesById(id);
+  return sale.length === 0 ? undefined
+    : sale.map(({ date, productId, quantity }) => ({ date, productId, quantity }));
+};
 
-module.exports = { 
-    getAllSales,
-    getSalesById,
- };
+const registerSales = async (sales) => {
+  const saleId = await salesModel.setSaleDataTime();
+  sales.forEach((sale) => salesModel.setSalesProducts({ saleId, ...sale }));
+  return {
+    id: saleId,
+    itemsSold: [...sales],
+  };
+};
+
+module.exports = {
+  getAllSales,
+  getSalesById,
+  registerSales,
+};
