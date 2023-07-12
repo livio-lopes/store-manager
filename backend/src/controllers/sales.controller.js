27ae356c553efd06278660e4c8bd3c5775d70e07
Lívio Lopes <antonio.lvio760@gmail.com>
2023-07-12 18:02:59 -0300

    feat: create updateQuantity on controller layer

--- backend/src/controllers/sales.controller.js
+++ backend/src/controllers/sales.controller.js
@@ -29,9 +29,24 @@ const deleteSalesById = async (req, res) => {
   return res.status(statusCode.NO_CONTENT).end();
 };
 
+const updateQuantity = async (req, res) => {
+  const { saleId, productId } = req.params;
+  const { quantity } = req.body;
+  const updatedQuantity = await salesService.updateQuantity(saleId, productId, quantity);
+  switch (updateQuantity) {
+    case 'SALEID':
+      return res.status(statusCode.NOT_FOUND).json(statusMessage.SALES_NOT_FOUND);
+    case 'PRODUCTID':
+      return res.status(statusCode.NOT_FOUND).json(statusMessage.PRODUCT_NOT_FOUND);
+    default:
+      return res.status(statusCode.OK).json(updatedQuantity);
+  }
+};
+
 module.exports = { 
   getAllSales,
   getSalesById,
   registerSales,
   deleteSalesById,
+  updateQuantity,
  };
